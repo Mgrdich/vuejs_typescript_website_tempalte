c363f5742a4ed0b50770a510754ff946537ffea6
Mgo <mgotm13@gmail.com>
2020-06-07 16:57:17 +0400

    Menu selection

--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container menu_container">
         <div class="top">
-            <h2>Menu</h2>
-            <div class="buttons">
+            <h2 v-scroll-reveal>Menu</h2>
+            <div class="buttons" v-scroll-reveal="{delay:300}">
                 <md-button
                 v-for="(btn,index) in buttons.data"
                 :key="index"
@@ -12,6 +12,28 @@
                     {{btn}}
                 </md-button>
             </div>
+            <div class="products_items" v-scroll-reveal="{delay:500}">
+                <md-card
+                        v-for="(product,index) in products"
+                        :key="product.name+index"
+                        v-show="buttons.active===product.type"
+                >
+                    <md-card-media md-ratio="16:9">
+                        <img :src="require(`../../assets/images/products/${product.img}`)" alt="">
+                    </md-card-media>
+                    <md-card-header>
+                        <h2 class="md-title">
+                            {{product.name}}
+                        </h2>
+                        <div class="md-subhead">
+                            <span>${{product.price}}</span>
+                        </div>
+                    </md-card-header>
+                    <md-card-content>
+                        {{product.desc}}
+                    </md-card-content>
+                </md-card>
+            </div>
         </div>
     </div>
 </template>
@@ -20,25 +42,32 @@
     import {Component, Vue} from 'vue-property-decorator';
     import {HttpResponse} from "vue-resource/types/vue_resource";
 
+    type productType = 'pizza'|'burgers'|'salat'|'dessert';
     type btns = {
-        data:Array<string>;
-        active:string;
+        data:Array<productType>;
+        active:productType
+    }
+    type product = {
+        img:string;
+        name:string;
+        price:number;
+        desc:string;
+        type:productType;
     }
 
     @Component
     export default class Menu extends Vue {
-        private  products:Array<any> = [];
+        private products:Array<product> = [];
         private buttons: btns = {
             data: [
                 'pizza',
                 'burgers',
-                'salad',
-                'desert'
+                'salat',
+                'dessert'
             ],
             active: 'pizza'
         };
-
-        created() {
+        created():void {
             this.$http.get('products.json')
                 .then((res: HttpResponse) => {
                     return res.json()
@@ -54,3 +83,15 @@
         }
     }
 </script>
+
+<style lang="scss" scoped>
+    .md-card {
+        width:32%;
+        margin-bottom: 20px;
+    }
+    .products_items {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap:wrap;
+    }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import Vue from 'vue';
 import App from './App.vue';
-import {MdButton,MdDrawer,MdContent,MdTabs} from 'vue-material/dist/components';
+import {MdButton,MdDrawer,MdCard} from 'vue-material/dist/components';
 import VueResource from 'vue-resource';
+import VueScrollReveal from 'vue-scroll-reveal';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
 
 Vue.use(MdButton);
 Vue.use(MdDrawer);
-Vue.use(MdContent);
-Vue.use(MdTabs);
+Vue.use(MdCard);
 Vue.use(VueResource);
+
+Vue.use(VueScrollReveal,{
+  duration:800,
+  scale:1,
+  distance:'50px'
+});
 
 (Vue as any).http.options.root = process.env.VUE_APP_API;
 
